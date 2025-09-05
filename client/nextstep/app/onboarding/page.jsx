a0a3e005b9fd5d7d637757cad29b8c1d3c2c7a05
Vansh-Bhatia-ctrl <vanshb066@gmail.com>
2025-09-05 23:07:13 +0530

    Connected the onboarding frontend with the backend and fetched the questions from the db

--- client/nextstep/app/onboarding/page.jsx
+++ client/nextstep/app/onboarding/page.jsx
@@ -1,31 +1,23 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth, useUser } from "@clerk/nextjs";
 import { useDispatch } from "react-redux";
 import { setUser } from "../redux/userSlice";
 
-const OPTIONS = [
-  {
-    id: 1,
-    option: "I've never coded before",
-  },
-  {
-    id: 2,
-    option: "Less than one year",
-  },
-  {
-    id: 3,
-    option: "1-3 years",
-  },
-  {
-    id: 4,
-    option: "3+ years",
-  },
-];
 const Page = () => {
   const { user, isLoaded } = useUser();
   const { getToken } = useAuth();
   const dispatch = useDispatch();
+
+  const [fetchedQuestions, setFetchedQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+
+  const [isLoadingQuestions, setIsLoadingQuestions] = useState(true);
+  const [userSaved, setUserSaved] = useState(false);
+
+  const totalQuestions = fetchedQuestions.length;
 
   const saveUserToDb = async () => {
     try {
@@ -51,6 +43,7 @@ const Page = () => {
 
       if (response.ok) {
         console.log("Data saved successfully.");
+        setUserSaved(true);
       }
       const data = await response.json();
       console.log(data);
@@ -68,41 +61,132 @@ const Page = () => {
     }
   };
 
+  const fetchQuestionsFromDb = async () => {
+    try {
+      const token = await getToken();
+
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_DEV_URL}/api/getQuestions/questions`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+      setFetchedQuestions(data.questionsData || []);
+      setIsLoadingQuestions(false);
+    } catch (error) {
+      console.log("Error fetching questions", error);
+      setIsLoadingQuestions(false);
+    }
+  };
+
   useEffect(() => {
-    saveUserToDb();
-  }, [user, isLoaded]);
+    if (isLoaded && user && !userSaved) {
+      saveUserToDb();
+    }
+  }, [user, isLoaded, userSaved]);
+
+  useEffect(() => {
+    if (isLoaded && user && userSaved) {
+      fetchQuestionsFromDb();
+    }
+  }, [isLoaded, user, userSaved]);
+
+  const handleNext = () => {
+    if (currentIndex >= totalQuestions - 1) {
+      setIsFinished(true);
+    } else {
+      setCurrentIndex((prev) => prev + 1);
+      setIsFinished(false);
+    }
+  };
+
+  if (!isLoaded) {
+    return (
+      <div className="min-h-screen w-screen bg-custom-gray-100 flex items-center justify-center">
+        <div className="text-white text-xl">Loading...</div>
+      </div>
+    );
+  }
+
+  if (isLoadingQuestions) {
+    return (
+      <div className="min-h-screen w-screen bg-custom-gray-100 flex items-center justify-center">
+        <div className="text-white text-xl">
+          Loading assessment questions...
+        </div>
+      </div>
+    );
+  }
+
+  console.log("fetched questions from DB: ", fetchedQuestions);
+  const currentQuestion = fetchedQuestions[currentIndex] ?? {
+    question: "",
+    options: [],
+  };
 
   return (
     <>
       <div className="min-h-screen w-screen bg-custom-gray-100 flex items-center justify-center overflow-x-hidden">
         <div className="w-[90vh] flex items-center justify-center">
-          <div className="bg-custom-gray-200 h-full w-[90%] md:w-[100%] p-9 rounded-xl border border-white -mt-25">
+          <div className="bg-custom-gray-200 h-full w-[90%] md:w-[100%] p-9 rounded-xl border-2 border-blue-700 -mt-25">
             <div className="text-center">
               <p className="text-blue-400 font-semibold text-4xl">
                 NextStep Assessment
               </p>
             </div>
 
-            <div className="text-center mt-5">
-              <p className="text-white text-lg font-semibold whitespace-pre-wrap">
-                How long have you been coding/programming?
-              </p>
-
-              {OPTIONS.map((option) => (
-                <div
-                  key={option.id}
-                  className="mt-5 bg-custom-gray-400 p-2 rounded-xl hover:bg-custom-gray-500 transition-all duration-300 ease-in-out cursor-pointer"
-                >
-                  <p className="text-white font-semibol text-sm text-start">
-                    {option.option}
+            {!isFinished && totalQuestions > 0 && (
+              <>
+                <div className="text-center mt-5">
+                  <p className="text-white text-lg font-semibold whitespace-pre-wrap">
+                    {currentQuestion.question}
                   </p>
-                </div>
-              ))}
-            </div>
 
-            <div className="mt-6 text-center">
-              <p className="text-slate-600 font-semibold">Question 1of 8</p>
-            </div>
+                  <div className="flex flex-col">
+                    {currentQuestion.options?.map((option, index) => (
+                      <button
+                        onClick={() => {
+                          handleNext();
+                          setIsAnswered(true);
+                        }}
+                        key={index}
+                        className="mt-5 bg-custom-gray-400 p-2 rounded-xl hover:bg-custom-gray-500 transition-all duration-300 ease-in-out cursor-pointer"
+                      >
+                        <p className="text-white font-semibol text-sm text-start">
+                          {option}
+                        </p>
+                      </button>
+                    ))}
+                  </div>
+                </div>
+                <div className="mt-6 text-center">
+                  <p className="text-slate-600 font-semibold">
+                    Question {currentIndex + 1} of {totalQuestions}
+                  </p>
+                </div>{" "}
+              </>
+            )}
+
+            {isFinished && (
+              <div className="mt-13 text-center">
+                <p className="text-green-400 font-bold text-xl whitespace-nowrap">
+                  🎉 Assessment Complete!
+                </p>
+                <p className="text-white mt-5 font-semibold ">
+                  Thank you for completing the assessment. Your responses have
+                  been submitted.
+                </p>
+                <button className="bg-blue-700 text-white font-semibold p-2 rounded-xl mt-9 hover:bg-blue-600 duration-300 transform-all ease-in-out cursor-pointer">
+                  Continue
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
